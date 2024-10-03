ab6cc1a7f7edb24e145ae4658e951e58ef38fcfb
Artem Sokolov <ArtemSokolovQA@yandex.ru>
2024-10-03 09:40:48 +0300

    Убрал ненужный принт

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -6,7 +6,6 @@ class TestBun:
     def test_get_name(self):
         bun = Bun('Bulka', 0)
         assert bun.get_name() == 'Bulka'
-        print(bun.get_name())
 
     def test_get_price(self):
         bun = Bun('dsdsjdsjdsjsdj', 0)
